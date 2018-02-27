afcdc83aa2dfbb498e92cc81eb20560001c2fad9
kartheepm <31505141+kartheepm@users.noreply.github.com>
2018-02-27 16:22:43 +0530

    Create ssd.c

---
+++ ssd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a,b[10],i,j=1;
+    scanf("%d",&a);
+    for(i=0;i<5;i++)
+    {
+      b[i]=a%10; 
+    }
+    for(i=0;i<5;i++)
+    {
+      if(b[i]==b[i+1]&&b[i]==b[i+2]&&b[i]==b[i+3]&&b[i]==b[i+4])
+      {j++;}
+    }
+    printf("%d",j);
+    return 0;
+}
