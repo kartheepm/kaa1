b0b530a856e11e2a6a08f224d9148ad1d2edb863
kartheepm <31505141+kartheepm@users.noreply.github.com>
2018-03-16 16:09:27 +0530

    Create thchar.c

---
+++ thchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+	char a[50];
+	int i,n;
+	scanf("%s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+		a[i]=a[i]+3;
+		if(a[i]=='X')
+		a[i]='A';
+		if(a[i]=='Y')
+		a[i]='B';
+		if(a[i]=='Z')
+		a[i]='C';
+	}
+	printf("%s",a);
+	return 0;
+}
